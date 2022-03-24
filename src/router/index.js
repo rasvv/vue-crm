edfefcd5d4b255a2a09242c0369c2cec0dec934f
rasvv@mail.ru <rasvv>
2022-03-24 18:03:58 +0300

    Lesson4

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
+		meta: {layout: "main"},
     component: Home,
   },
   {
@@ -24,6 +25,48 @@ const routes = [
 		meta: {layout: "main"},
     component: () =>
       import("../views/Categories.vue"),
+  },
+  {
+    path: "/detail",
+    name: "detail",
+		meta: {layout: "main"},
+    component: () =>
+      import("../views/DetailRecord.vue"),
+  },
+  {
+    path: "/history",
+    name: "history",
+		meta: {layout: "main"},
+    component: () =>
+      import("../views/History.vue"),
+  },
+  {
+    path: "/planning",
+    name: "planning",
+		meta: {layout: "main"},
+    component: () =>
+      import("../views/Planning.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+		meta: {layout: "main"},
+    component: () =>
+      import("../views/Profile.vue"),
+  },
+  {
+    path: "/record",
+    name: "record",
+		meta: {layout: "main"},
+    component: () =>
+      import("../views/Record.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+		meta: {layout: "empty"},
+    component: () =>
+      import("../views/Register.vue"),
   },
 ];
 
